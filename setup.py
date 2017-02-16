fde92e93396267f2def2abcdc5d5a73c064e1059
Jian Dai <daij12@gene.com>
2017-02-16 15:59:59 -0800

    minimal pack

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='pythonPlayGround',
+	version='0.1',
+	description='play ground for python',
+	url='http://rkalva89828.kau.roche.com/daij12/pythonPlayGround',
+	author='Jian Dai',
+	author_email='daij12@gene.com',
+	license='MIT',
+	packages=['pythonPlayGround'],
+	zip_safe=False)
